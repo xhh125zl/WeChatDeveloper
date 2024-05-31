376f815490497f6a166c1e98e4c8c35b2137a2af
xiehh <1042370690@qq.com>
2024-06-01 00:29:17 +0800

    新增分账相关接口

--- WePayV3/Ecommerce.php
+++ WePayV3/Ecommerce.php
@@ -72,7 +72,50 @@ class Ecommerce extends BasicWePay
             ]
         ];
 
-        $path = "v3/merchant/media/upload";
+        $path = "/v3/merchant/media/upload";
+        return $this->doRequest('POST', $path, json_encode($data, JSON_UNESCAPED_UNICODE), true);
+    }
+
+    // ----------------------------------------------------------------
+    // 分账处理
+    /**
+     * 请求分账
+     * @param array $data 请求参数
+     * @return array
+     * @throws \WeChat\Exceptions\InvalidResponseException
+     * @document https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter7_4_1.shtml
+     */
+    public function profitShare($data)
+    {
+        $path = "/v3/ecommerce/profitsharing/orders";
+        return $this->doRequest('POST', $path, json_encode($data, JSON_UNESCAPED_UNICODE), true);
+    }
+
+    /**
+     * 查询分账结果
+     * @param array $data 请求参数
+     * @return array
+     * @throws \WeChat\Exceptions\InvalidResponseException
+     * @document https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter7_4_2.shtml
+     */
+    public function queryProfitShare($data)
+    {
+        $path = "/v3/ecommerce/profitsharing/orders";
+
+        $params = http_build_query($data);
+        return $this->doRequest('GET', "{$path}?{$params}", '', true);
+    }
+
+    /**
+     * 完结分账
+     * @param array $data 请求参数
+     * @return array
+     * @throws \WeChat\Exceptions\InvalidResponseException
+     * @document https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter7_4_5.shtml
+     */
+    public function finishProfitShare($data)
+    {
+        $path = "/v3/ecommerce/profitsharing/finish-order";
         return $this->doRequest('POST', $path, json_encode($data, JSON_UNESCAPED_UNICODE), true);
     }
 }
